ec0c27a4298bf7b98f0b210b8c350b3a74f092ae
JCR <jincanran@gmail.com>
2016-04-03 22:20:54 +0800

    4.3change database

--- news/models.py
+++ news/models.py
@@ -1,27 +1,25 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=30)
-    password = models.CharField(max_length=50)
-    
-
-class Author(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=40)
-    email = models.EmailField()
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    authors = models.ManyToManyField(Author)
-    User = models.ForeignKey(User)
-    publication_date = models.DateField()
+	name = models.CharField(max_length=30)
+	password = models.CharField(max_length=50)
+	email = models.CharField(max_length=100)
+	message = models.CharField(max_length=512)
+	interest = models.CharField(max_length=512)
 	
 class News(models.Model):
 	title = models.CharField(max_length=100)
 	data = models.CharField(max_length=100)
 	author = models.CharField(max_length=100)
-	text = models.CharField(max_length=1000)
+	abstract = models.CharField(max_length=10000)
+	link = models.CharField(max_length=1000)
     
+class Detail(models.Model):
+	title = models.CharField(max_length=100)
+	data = models.CharField(max_length=100)
+	author = models.CharField(max_length=100)
+	text = models.CharField(max_length=10000)
+	link = models.CharField(max_length=1000)
+	mediaurl = models.CharField(max_length=1000)
